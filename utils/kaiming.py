51f44394286fcd4ba7522b2e6805c738a73c47f3
Alen <1121451973@qq.com>
2026-09-18 15:04:37 +0800

    solve init2

--- utils/kaiming.py
+++ utils/kaiming.py
@@ -3,11 +3,13 @@ import torch.nn as nn
 
 def init_weights(m):
     if isinstance(m, nn.Conv2d):
-        nn.init.kaiming_normal_(m.weight, mode="fan_out", nonlinearity="relu")
+        nn.init.kaiming_normal_(m.weight, mode="fan_in", nonlinearity="relu")
 
         if m.bias is not None:
-            nn.init.constant_(m.bias, 0)
+            nn.init.zeros_(m.bias)
 
     elif isinstance(m, nn.Linear):
-        nn.init.normal_(m.weight, 0, 0.01)
-        nn.init.constant_(m.bias, 0)
+        nn.init.normal_(m.weight, mean=0.0, std=0.01)
+
+        if m.bias is not None:
+            nn.init.zeros_(m.bias)
